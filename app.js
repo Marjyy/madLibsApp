aa9a277b5269306fb0c3aaf8c9737d91b3e4ed3a
Marjorie <101886863+Marjyy@users.noreply.github.com>
2022-03-30 13:46:24 +0200

    Travelling is Nice

---
+++ app.js
@@ -0,0 +1,22 @@
+let originalStory="You decide to leave your job as occupation and your country.You choose to travel by meansOfTransportation to the country of your dream: your favorite country. You take with you your favorite object";
+
+
+var yourName=prompt("Enter your name: ");
+var occupation=prompt("Enter an occupation: ");
+var country=prompt("Enter a country: ");
+var meansOfTransportation=prompt("Enter a means of transportation: ");
+var favoriteCountry=prompt("Enter the name of your favorite country: ");
+var favoriteObject=prompt("Enter the name of your favorite object: ");
+
+let userInputs=[yourName,occupation,country,meansOfTransportation,favoriteCountry,favoriteObject];
+
+
+let userStory= (`${userInputs[0]} decide to leave your job as ${userInputs[1]} and ${userInputs[2]}. You choose to travel by ${userInputs[3]} to the country of your dream: ${userInputs[4]}. You take with you ${userInputs[5]}`);
+
+document.write(userStory);
+
+
+
+
+
+//You decide to leave your job as occupation and your country.You choose to travel by meansOfTransportation to the country of your dream: your favorite country. You take with you your favorite object"
